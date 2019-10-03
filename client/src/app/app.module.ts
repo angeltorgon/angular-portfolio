b8e9f0e4dace53cdc7e1d363d17709f860655d76
angel-torres <alas4224@gmail.com>
2019-10-03 15:29:53 -0500

    added skills component where I will be adding a list of all my skills

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
